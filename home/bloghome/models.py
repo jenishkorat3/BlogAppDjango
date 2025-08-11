cf2a938e5de7c17e8f1ec3041681be81fdb4cefc
Jenish Korat <jeka@odoo.com>
2025-08-11 10:21:27 +0530

    [ADD] blogapp: added bloghome view.

--- home/bloghome/models.py
+++ home/bloghome/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=15)
+    message = models.TextField()
+    timeStamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Message from {self.name} - {self.email}'
--- home/bloghome/views.py
+++ home/bloghome/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, HttpResponse
+from .models import Contact
 
 # Create your views here.
 def home(request):
     return render(request, 'bloghome/home.html')
 
 def contact(request):
-    return render(request, 'bloghome/contact.html')
+    isSubmitted = False
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        contact = Contact(name=name, email=email, phone=phone, message=message)
+        contact.save()
+        isSubmitted = True
+    return render(request, 'bloghome/contact.html', {'isSubmitted': isSubmitted})
 
 def about(request):
     return render(request, 'bloghome/about.html')
-
