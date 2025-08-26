19a21f441608a0f2c7994d2334b2238becda0fac
Jenish Korat <jeka@odoo.com>
2025-08-26 12:23:10 +0530

    [ADD] blogapp: added signup form.

--- home/bloghome/views.py
+++ home/bloghome/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Contact
 from blog.models import Post
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -30,3 +32,34 @@ def search(request):
         posts = Post.objects.filter(title__icontains=query) | Post.objects.filter(content__icontains=query)
 
     return render(request, 'bloghome/search.html', {'query' : query ,'posts': posts})
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        if len(username)<10:
+            messages.error(request, " Your user name must be under 10 characters")
+            return redirect('home')
+
+        if not username.isalnum():
+            messages.error(request, " User name should only contain letters and numbers")
+            return redirect('home')
+
+        if (pass1!= pass2):
+             messages.error(request, " Passwords do not match")
+             return redirect('home')
+
+        user = User.objects.create_user(username, email, pass1)
+        user.first_name = fname
+        user.last_name = lname
+        user.save()
+        messages.success(request, "Your iCoder account has been sucessfully created.")
+
+        return redirect('home')
+    else:
+        return HttpResponse('404 - Not Allowed')
--- home/home/settings.py
+++ home/home/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Manually Added Settings
+import os
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
