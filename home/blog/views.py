66504e6e1634a69f0d695f1d94507401e1369bfe
Jenish Korat <jeka@odoo.com>
2025-09-03 12:27:59 +0530

    [ADD] blogapp: added replys

--- home/blog/views.py
+++ home/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import BlogComment, Post
 from django.contrib import messages
+from blog.templatetags import extras
 
 # Create your views here.
 def index(request):
@@ -9,8 +10,15 @@ def index(request):
 
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
-    comments = BlogComment.objects.filter(post=post).order_by('-timestamp')
-    return render(request, 'blog/post.html', {'post': post, 'comments': comments, 'user': request.user})
+    comments = BlogComment.objects.filter(post=post, parent=None).order_by('-timestamp')
+    replies = BlogComment.objects.filter(post=post).exclude(parent=None).order_by('-timestamp')
+    replyDict = {}
+    for reply in replies:
+        if reply.parent.sno not in replyDict:
+            replyDict[reply.parent.sno] = [reply]
+        else:
+            replyDict[reply.parent.sno].append(reply)
+    return render(request, 'blog/post.html', {'post': post, 'comments': comments, 'user': request.user, 'replyDict': replyDict})
 
 def postComment(request):
     if request.method == "POST":
@@ -18,8 +26,16 @@ def postComment(request):
         user = request.user
         postSno = request.POST.get('postSno')
         post = Post.objects.filter(sno=postSno).first()
-        if post:
+        parentSno = request.POST.get('parentSno')
+
+        if post and parentSno == "":
             comment = BlogComment(comment=comment, user=user, post=post)
             comment.save()
             messages.success(request, "Your comment has been posted successfully")
+        else:
+            parent = BlogComment.objects.filter(sno=parentSno).first()
+            comment = BlogComment(comment=comment, user=user, post=post, parent=parent)
+            comment.save()
+            messages.success(request, "Your reply has been posted successfully")
+
     return redirect("blogPost", slug=post.slug)
